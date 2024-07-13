faa97d170e80b361a1ac66bb5a2b9a0a97dbd27d
Vishal Deval <vishaldeval098@gmail.com>
2024-07-13 11:46:10 +0530

    separate html css and js files

---
+++ port2.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName('insta')[0].addEventListener('click', function () {
+    open('https://www.instagram.com/vdeval016/')
+})
+document.getElementsByClassName('linkden')[0].addEventListener('click', function () {
+    open('https://www.linkedin.com/in/vishal-deval-0a1283296?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app')
+})
+document.getElementsByClassName('git')[0].addEventListener('click', function () {
+    open('https://github.com/VishalDeval')
+})
